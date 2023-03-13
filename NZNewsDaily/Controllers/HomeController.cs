b49afd6f7181b97348b1349e7b524a0d14ecb86a
Sean Crump <116034584+Ess-See76@users.noreply.github.com>
2023-03-14 12:56:27 +1300

    Add project files.

---
+++ NZNewsDaily/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZNewsDaily.Models;
+using System.Diagnostics;
+
+namespace NZNewsDaily.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult NewZealand()
+        {
+            return View();
+        }
+
+        public IActionResult Technology()
+        {
+            return View();
+        }
+
+        public IActionResult Sports()
+        { 
+            return View(); 
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
